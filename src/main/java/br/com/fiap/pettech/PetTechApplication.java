d9e690f8658e6ad16563a5f26f46a536231eb1d3
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-07-26 21:26:54 -0300

    Continuidade projeto persistência

--- src/main/java/br/com/fiap/pettech/PetTechApplication.java
+++ src/main/java/br/com/fiap/pettech/PetTechApplication.java
@@ -1,13 +1,28 @@
 package br.com.fiap.pettech;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PetTechApplication {
+    
+//    public PetTechApplication() throws SQLException {
+//
+//        final String url = "jdbc:postgresql://localhost:5432/pettech";
+//        final String user = "user";
+//        final String password = "123456";
+//
+//        Connection connnection = DriverManager.getConnection(url, user, password);
+//
+//        System.out.println("Conexão efetuada com sucesso!");
+//        connnection.close();
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(PetTechApplication.class, args);
     }
+    
 
 }
